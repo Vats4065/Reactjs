dd29b43e731e506d9cb5f45280b347de3b11a794
vats4065 <vatsal.r.lakhani2626@gmail.com>
2024-04-12 13:07:10 +0530

    router

--- intro-router-react/src/App.js
+++ intro-router-react/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Error from "./Components/Error";
+import Menu from "./Components/Menu";
 
 function App() {
+  function name() {
+    return <h1>Hello from name page</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu></Menu>
+      <Switch>
+        <Route exact path={["/", "/about"]} component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/contact/name" component={name} />
+        <Route component={Error}></Route>
+      </Switch>
+    </>
   );
 }
 
