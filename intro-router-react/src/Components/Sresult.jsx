9ec1be17e48bca7dd9dbbe3702731ab360d9e808
vats4065 <vatsal.r.lakhani2626@gmail.com>
2024-04-12 18:30:34 +0530

    up

--- intro-router-react/src/Components/Sresult.jsx
+++ intro-router-react/src/Components/Sresult.jsx
@@ -1,9 +1,11 @@
- function Sresult (props) {
-    const imgsrc=`https://source.unsplash.com/600×900/?${props.name}`
+ function Sresult () {
+    const imgsrc=`https://source.unsplash.com/user/erondu/1600×900`
     return (
       <>
         <img src={imgsrc} alt="not found" />
       </>
     );
   };
-export default Sresult
+
+
+  export default Sresult
